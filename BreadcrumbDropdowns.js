78d17e3c59aa0c00b8aae57187036d33e861a683
Robin Sallis <Toutouwai@users.noreply.github.com>
2025-08-11 12:40:12 +1200

    v0.3.13
    Scrollbars in dropdown for Konkat theme.

--- BreadcrumbDropdowns.js
+++ BreadcrumbDropdowns.js
@@ -1,17 +1,18 @@
-(function($) {
-
-	$(function(){
-		$(document).click(function(event) {
-			var $dropdowns = $('.uk-breadcrumb .breadcrumb-dropdown');
-			var $toggle = $(event.target).closest('.dropdown-toggle');
-			if($toggle.length) {
-				var $related_dropdown = $toggle.siblings('.breadcrumb-dropdown');
-				$related_dropdown.toggleClass('visible');
-				$dropdowns.not($related_dropdown).removeClass('visible');
-			} else {
-				$dropdowns.removeClass('visible');
+$(function() {
+	$(document).on('click', function(event) {
+		const $dropdowns = $('.uk-breadcrumb .breadcrumb-dropdown');
+		const $toggle = $(event.target).closest('.dropdown-toggle');
+		if($toggle.length) {
+			const $related_dropdown = $toggle.siblings('.breadcrumb-dropdown');
+			$related_dropdown.toggleClass('visible');
+			$dropdowns.not($related_dropdown).removeClass('visible');
+			// For Konkat theme, calculate max-height dynamically to allow for variable offset top
+			if($('body').hasClass('AdminThemeUikitDefault')) {
+				const offset = $related_dropdown.offset().top + 10;
+				$related_dropdown.css('max-height', `calc(100vh - ${offset}px)`);
 			}
-		});
+		} else {
+			$dropdowns.removeClass('visible');
+		}
 	});
-
-}(jQuery));
+});
